894bcbde7f089b5df770a37a32615b9e08f364fc
James Thayer <jrthayer@users.noreply.github.com>
2023-10-31 18:56:37 -0700

    Finish meet the party section
    Reorganize several files and do cleanup on necessary code

---
+++ navigation.js
@@ -0,0 +1,4 @@
+//Event listener for mobile navigation
+document.querySelector("#mobile-nav-button").addEventListener("click", () => {
+    document.querySelector(".social-links").classList.toggle("flex");
+});
