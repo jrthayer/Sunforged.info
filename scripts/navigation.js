956202c7ea7362564916768ea40a18e2e629ee23
James Thayer <jrthayer@users.noreply.github.com>
2024-09-13 17:22:32 -0700

    Update mobile social links
    Now prevents scrolling when nav is open, Changes the hamburger menu to an x, and has the highest z index

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,4 +1,6 @@
 //Event listener for mobile navigation
 document.querySelector("#mobile-nav-button").addEventListener("click", () => {
     document.querySelector(".social-links").classList.toggle("flex");
+    document.querySelector("body").classList.toggle("hiddenY");
+    document.querySelector(".floating-button__center").classList.toggle("open");
 });
